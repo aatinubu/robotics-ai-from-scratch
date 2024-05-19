0c502a839b54f407819ec74cc2d679ae5b78c032
Akolade Tinubu <akolade.tinubu@gmail.com>
2024-05-19 12:24:20 +0000

    chore: Update zsh plugins and editor font size
    The zsh plugins `zsh-autocomplete` and `zsh-syntax-highlighting` have been updated to their latest versions. Additionally, the font size for the editor, debug console, and integrated terminal has been increased to 18.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,9 +12,9 @@ sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1
     -p git \
     -p ssh-agent \
     -p https://github.com/zsh-users/zsh-autosuggestions \
-    -p https://github.com/marlonrichert/zsh-autocomplete \
     -p https://github.com/zsh-users/zsh-syntax-highlighting \
-    -p https://github.com/MichaelAquilina/zsh-you-should-use
+    -p https://github.com/MichaelAquilina/zsh-you-should-use \
+    -p https://github.com/marlonrichert/zsh-autocomplete 
 
 mkdir -p /home/$USERNAME/.cache/zsh
 eval "$(ssh-agent -s)"
